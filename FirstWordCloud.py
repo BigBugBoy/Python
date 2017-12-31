a1c6e51feafd844dc762812ac88c2a3476dfa5cb
Wang wenjun <sh118wwj@icloud.com>
2017-12-31 11:21:23 +0800

    AlterFilePath

---
+++ FirstWordCloud.py
@@ -0,0 +1,18 @@
+from wordcloud import WordCloud,ImageColorGenerator
+import matplotlib.pyplot as plt
+import jieba
+with open('19Da.txt','r',encoding = 'utf-8') as f:
+    mytext=f.read()
+mytext = " ".join(jieba.cut(mytext))
+font = r'C:\Windows\Fonts\simfang.ttf'
+backgroud_Image = plt.imread('tree.jpg')
+wc = WordCloud(collocations=False,background_color = 'white',mask = backgroud_Image, font_path=font, width=2400, height=2400, margin=3).generate(mytext.lower())
+image_colors = ImageColorGenerator(backgroud_Image)
+wc.recolor(color_func = image_colors)
+plt.imshow(wc)
+plt.axis("off")
+plt.show()
+wc.to_file('19DaTree.png')
+
+
+
